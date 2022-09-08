4a394c95cdc35729bdc5f9db42973a6a0eb21ce3
Isaac Muniz <isaacmuniz@protonmail.com>
2022-09-07 23:36:38 -0300

    Double check same function

--- util.test.js
+++ util.test.js
@@ -2,6 +2,8 @@ const { generateText } = require('./util')
 
 test('should output name and age', () => {
   const text = generateText('Isaac', 23)
-
   expect(text).toBe('Isaac (23 years old)')
+
+  const text1 = generateText('', null)
+  expect(text1).toBe(' (null years old)')
 })
